bc18a383f3e6e5d9723f6074ac9636ec200e2a14
Ruan Rodrigues <rupriwell123@outlook.com>
2025-02-15 13:42:16 -0300

    Projeto create

---
+++ src/main/java/com/ruandev/br/projetogastos/ProjetogastosApplication.java
@@ -0,0 +1,13 @@
+package com.ruandev.br.projetogastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetogastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetogastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruandev/br/projetogastos/ProjetogastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruandev.br.projetogastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetogastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
